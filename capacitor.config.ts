007770dcdfc125d1c42867bd549d17e0f78a886f
abhay jani <abhayjaniit@gmail.com>
2023-05-16 23:37:24 +0530

    fix for apk build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,9 +4,7 @@ const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'YourPaids',
   webDir: 'dist',
-  server: {
-    androidScheme: 'https'
-  }
+  bundledWebRuntime: false
 };
 
 export default config;
